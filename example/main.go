6596e25fd19db40b7fd6bb92a3731d7a1a1ad92e
zjyl1994 <me@zjyl1994.com>
2019-07-10 18:02:29 +0800

    add example

--- example/main.go
+++ example/main.go
@@ -1,27 +1,62 @@
 package main
 
-import (
-	"os"
+import "github.com/zjyl1994/uroncha"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/joho/godotenv/autoload"
-	"github.com/sirupsen/logrus"
-)
+var logger = uroncha.Logger
 
 func main() {
-	logger := logrus.New()
-	logger.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat:  "2006-01-02 15:04:05.000",
-		DisableTimestamp: false,
-		FullTimestamp:    true,
-	})
-	logger.Println("Uroncha Test Server")
-	router := gin.Default()
-	router.GET("/test", TestHandler)
-	err := router.Run(":8080")
-	logrus.Println("Server", err)
+	logger.Println("Uroncha Example")
+	uroncha.Handle("GET", "/ping", uroncha.Rules{}, pingHandler)
+	uroncha.Handle("GET", "/get", uroncha.Rules{
+		QueryString: uroncha.Rule{
+			"type": []string{"required", "in:1,2,3,4,5"},
+			"ts":   []string{"unix_timestamp"},
+		},
+	}, getHandler)
+	uroncha.Handle("POST", "/post", uroncha.Rules{
+		Body: uroncha.Rule{
+			"data1":       []string{"required"},
+			"data2.data3": []string{"required", "integer"},
+			"data4":       []string{"string", "in:d1,d2,d3"},
+		},
+	}, postHandler)
+	uroncha.Run()
 }
 
-func TestHandler(c *gin.Context) {
-	c.JSON(200, os.Getenv("TEST"))
+//EXAMPLE: GET http://127.0.0.1:8000/ping
+func pingHandler(c *uroncha.Context, params uroncha.Datas) (interface{}, uroncha.Error) {
+	return uroncha.H{
+		"name":  "URONCHA",
+		"debug": uroncha.IsDebug(),
+	}, uroncha.NoError
+}
+
+//EXAMPLE: GET http://127.0.0.1:8000/get?type=2&ts=1562752722
+func getHandler(c *uroncha.Context, params uroncha.Datas) (interface{}, uroncha.Error) {
+	return uroncha.H{
+		"type": params.QueryString["type"],
+		"ts":   params.QueryString["ts"],
+	}, uroncha.NoError
+}
+
+/*
+EXAMPLE: POST http://127.0.0.1:8000/post
+{
+	"data1":"asdfghjkl",
+	"data2":{
+		"data3":2
+	},
+	"data4":"d3"
+}
+*/
+func postHandler(c *uroncha.Context, params uroncha.Datas) (interface{}, uroncha.Error) {
+	body := params.Body
+	data1 := body.Get("data1").MustString()
+	data3 := body.Get("data2").Get("data3").MustInt()
+	data4 := body.Get("data4").MustString()
+	return uroncha.H{
+		"data1": data1,
+		"data3": data3,
+		"data4": data4,
+	}, uroncha.NoError
 }
